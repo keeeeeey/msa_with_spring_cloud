6031ff9c99a06922e98ca7d09007027de8b3784d
keeeeeey <sseioul@naver.com>
2023-03-07 09:55:18 +0900

    FEAT: create apigateway project

---
+++ lab1/second-servicee/src/main/java/com/example/secondservicee/SecondServiceeApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondservicee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondServiceeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondServiceeApplication.class, args);
+    }
+
+}
